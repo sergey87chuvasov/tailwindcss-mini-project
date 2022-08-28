5fab03f8ec55ecdfc07dc32f6bc3954d642bc1d3
Sergey87 <serge.techno@gmail.com>
2022-08-28 20:35:58 +0300

    added navbar and burger menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./*.html'],
+
   theme: {
-    extend: {},
+    screens: {
+      lg: { max: '992px' },
+      md: { max: '768px' },
+      sm: { max: '480px' },
+    },
+    container: {
+      padding: '20px',
+      center: true,
+    },
+    extend: {
+      colors: {
+        lightblack: '#4D4244',
+        lightred: '#FF0D38',
+        darkred: '#D70026',
+        lightgray: '#7474747',
+        darkgray: '#272727',
+      },
+    },
   },
   plugins: [],
 };
