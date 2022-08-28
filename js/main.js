3552644d19bcba108f9098d8d3c96913e5f0f884
Sergey87 <serge.techno@gmail.com>
2022-08-29 00:20:35 +0300

    finished project added dark mode

--- js/main.js
+++ js/main.js
@@ -10,3 +10,27 @@ function burgerMenu() {
 }
 
 burgerMenu();
+
+// dark mode
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+if (
+  localStorage.theme === 'dark' ||
+  (!('theme' in localStorage) &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches)
+) {
+  document.documentElement.classList.add('dark');
+} else {
+  document.documentElement.classList.remove('dark');
+}
+
+const modeBtn = document.querySelector('#mode');
+
+modeBtn.addEventListener('click', () => {
+  document.querySelector('html').classList.toggle('dark');
+
+  if (document.querySelector('html').classList.contains('dark')) {
+    localStorage.theme = 'dark';
+  } else {
+    localStorage.theme = 'light';
+  }
+});
